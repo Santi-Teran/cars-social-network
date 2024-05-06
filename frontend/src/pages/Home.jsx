bde359a5d7339e04df0d70c541df5a9032081681
Santi Teran <santiagoteranmatias@gmail.com>
2024-05-06 10:58:43 -0300

    share posts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Stories from '../components/Stories';
 import Posts from '../components/Posts';
+import Share from '../components/Share';
 
 const Home = () => {
   return (
     <div className='bg-gray-100 flex6 py-5 px-16'>
       <Stories />
+      <Share />
       <Posts />
     </div>
   )
