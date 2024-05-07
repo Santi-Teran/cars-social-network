4ed604f2387dc03cca950d4588ce84d710facf46
Santi Teran <santiagoteranmatias@gmail.com>
2024-05-07 10:14:08 -0300

    profile

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,14 @@
-export const getUser = (req, res) => {
+import {db} from '../connect.js';
+import jwt from 'jsonwebtoken';
 
+export const getUser = (req, res) => {
+  const userId = req.params.userId;
+  const q = 'SELECT * FROM users WHERE id=?'
+
+  db.query(q, [userId], (error, data) => {
+    if(error) return res.status(500).json(error)
+    const { password, ...info} = data[0];
+    return res.json(info);
+  })
 }
 
